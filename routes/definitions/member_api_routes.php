c44bfe4fee7198ddc37108b7582b71bdbf86b911
jhuanca <huancajuan863@gmail.com>
2026-05-31 17:09:06 -0400

    arreglo prospecto

--- routes/definitions/member_api_routes.php
+++ routes/definitions/member_api_routes.php
@@ -22,7 +22,6 @@ use App\Http\Controllers\Api\InvoiceController;
 use App\Http\Controllers\Api\WalletWithdrawController;
 use App\Http\Controllers\Api\WalletController;
 use App\Http\Controllers\Api\WithdrawalController;
-use App\Http\Controllers\ProspectosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -74,8 +73,7 @@ return function (bool $v1Auth = false): void {
     Route::get('/packages', [PackageCatalogController::class, 'index']);
     Route::get('/countries', [PublicCountryController::class, 'index']);
 
-    Route::apiResource('prospectos', ProspectosController::class)
-        ->names($v1Auth ? 'v1.prospectos' : 'prospectos');
+  
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/me/binary-placement', [BinaryPlacementSelfController::class, 'store']);
